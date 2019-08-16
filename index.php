3bd42a60eed2df46458477e164c8c6594673d6f6
MauricioBasar <cristianmauriciobasar@gmail.com>
2019-08-16 01:46:39 -0300

    Deje todo Preparado YAAA Soy Cristian

--- index.php
+++ index.php
@@ -2,13 +2,17 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Alizon.com</title>
+    <title>Alizon.com El Comercio Que Siempre Buscaste</title>
+    <meta name="content" content="Encontrá Los Mejores Productos De Las Mejores Marcas Solo En Alizon.Com. Comprá en 12 y 18 Cuotas s/Interés. Ahora 12. Entrega Inmediata. Los Mejores Descuentos. Envíos a Todo el Pais...">
+    <meta name="author" content="GrupoHouse">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/estilos.css">
-
+    <script src="js/jquery-3.4.1.min.js" charset="utf-8"></script>
   </head>
   <body>
-    <p>HOLa Mundo</p>
+    <div class="contenedor">
+      <?php require_once('slider.php'); ?>
+    </div>
 
   </body>
 </html>
