932fb5bd83ad2008b0e0a6fd38cd4c6709f3463c
MauricioBasar <cristianmauriciobasar@gmail.com>
2019-08-16 12:00:30 -0300

    Subida Cristiannn

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Alizon.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/estilos.css">
+
+  </head>
+  <body>
+    <p>HOLa Mundo</p>
+
+  </body>
+</html>
