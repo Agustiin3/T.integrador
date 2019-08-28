63402fcdff4a6056c45fe2305efc4f4c6accfa19
MauricioBasar <cristianmauriciobasar@gmail.com>
2019-08-28 03:07:33 -0300

    ahi acomode un poco esta parte jonas asi podes trabajar mejor

--- centrodeayuda.php
+++ centrodeayuda.php
@@ -2,155 +2,244 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Alizon.com El Comercio Que Siempre Buscaste</title>
-    <meta name="content" content="Encontrá Los Mejores Productos De Las Mejores Marcas Solo En Alizon.Com. Comprá en 12 y 18 Cuotas s/Interés. Ahora 12. Entrega Inmediata. Los Mejores Descuentos. Envíos a Todo el Pais...">
+    <title>Alizon.com Centro de Atención al Cliente</title>
+    <meta name="content" content="Centro de Atención al ClienteEncontrá Los Mejores Productos De Las Mejores Marcas Solo En Alizon.Com. Envíos a Todo el Pais...">
     <meta name="author" content="GrupoHouse">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/estilos.css">
   </head>
   <body>
     <div class="contenedor">
-    <h1> Bienvenido al Centro de Atención al cliente </h1>
+    <hgroup>
+      <h1>Bienvenido al Centro de Atención al cliente</h1>
+      <h2>Lo más consultado</h2>
+    </hgroup>
 
-    <h3>Lo más consultado</h3>
-
-    <div class="preguntas-frecuentes">
+    <div class="lomasconsultado">
     <ul>
-      <li>Consultar-donde-se-encuentra-pedido</li>
-      <li>Servicios-tecnicos-oficiales</li>
-      <li>Consultar-si-ya-puedo-retirar-en-sucursal</li>
+      <li>Consultar donde se encuentra mi pedido</li>
+      <li>Servicios técnicos oficiales</li>
+      <li>Consultar si ya puedo retirar en sucursal</li>
     </ul>
-
     </div>
 
     <h2>¿Cómo podemos ayudarte?</h2>
 
-    <div class="servicios-de-ayuda">
-
-      <ul>
-        <li>
-        <div class="compras">
-        <i class="fas fa-shopping-bag"></i>
-        </div>
-          <h1>Compras</h1>
-        <a href="cupon-descuento">Tengo un cupón descuento</a>
-          <a href="costo-de-envio">Conocé los costos de envío</a>
-            <a href="compra-registrada">¿Mi compra está registrada?</a>
-              <a href="como-comprar-online">¿Cómo comprar en garbarino.com?</a>
-                <a href="promociones-medios-de-pago">Promociones y medios de pago</a>
-                  <a href="Retiro-sucursal">Retiro en sucursal</a>
-                    <a href="compre-licensia-digital">Compré una licensia digital</a>
-                      <a class="masinfo" href="masinfo">Más información</a>
-
-      </li>
-
-      <li>
-      <div class="pago">
-        <i class="fas fa-file-invoice-dollar"></i>
-      </div>
-        <h1>Pagos y facturación</h1>
-        <a href="cupon-descuento">Tengo un cupón descuento</a>
-          <a href="costo-de-envio">Conocé los costos de envío</a>
-            <a href="compra-registrada">¿Mi compra está registrada?</a>
-              <a href="como-comprar-online">¿Cómo comprar en garbarino.com?</a>
-                <a href="promociones-medios-de-pago">Promociones y medios de pago</a>
-                  <a href="Retiro-sucursal">Retiro en sucursal</a>
-                    <a href="compre-licensia-digital">Compré una licensia digital</a>
-                      <a class="masinfo" href="masinfo">Más información</a>
-    </li>
-
-
-    <li>
-    <div class="entrega">
-    <i class="fas fa-truck"></i>
-    </div>
-      <h1>Entregas</h1>
-    <a href="cupon-descuento">Tengo un cupón descuento</a>
-      <a href="costo-de-envio">Conocé los costos de envío</a>
-        <a href="compra-registrada">¿Mi compra está registrada?</a>
-          <a href="como-comprar-online">¿Cómo comprar en garbarino.com?</a>
-            <a href="promociones-medios-de-pago">Promociones y medios de pago</a>
-              <a href="Retiro-sucursal">Retiro en sucursal</a>
-                <a href="compre-licensia-digital">Compré una licensia digital</a>
-                  <a class="masinfo" href="masinfo">Más información</a>
-
-    </li>
-
-    <li>
-    <div class="sucursales">
-    <i class="fas fa-store"></i>  
-    </div>
-      <h1>Sucursales</h1>
-    <a href="lista-sucursal">Listados de Sucursales:Días y horarios </a>
-
-    </li>
-
-    <li>
-    <div class="garantias-servicios">
-    <i class="fas fa-tools"></i>
-    </div>
-      <h1>Garantias servicios técnicos e Instalaciones</h1>
-    <a href="cupon-descuento">Tengo un cupón descuento</a>
-      <a href="costo-de-envio">Conocé los costos de envío</a>
-        <a href="compra-registrada">¿Mi compra está registrada?</a>
-          <a href="como-comprar-online">¿Cómo comprar en garbarino.com?</a>
-            <a href="promociones-medios-de-pago">Promociones y medios de pago</a>
-              <a href="Retiro-sucursal">Retiro en sucursal</a>
-                <a href="compre-licensia-digital">Compré una licensia digital</a>
-                  <a class="masinfo" href="masinfo">Más información</a>
-
-    </li>
-
-    <li>
-    <div class="cambios y dev">
-    <i class="fas fa-retweet"></i>
-    </div>
-      <h1>Cambios y devoluciones</h1>
-    <a href="cupon-descuento">Tengo un cupón descuento</a>
-      <a href="costo-de-envio">Conocé los costos de envío</a>
-        <a href="compra-registrada">¿Mi compra está registrada?</a>
-          <a href="como-comprar-online">¿Cómo comprar en garbarino.com?</a>
-            <a href="promociones-medios-de-pago">Promociones y medios de pago</a>
-              <a href="Retiro-sucursal">Retiro en sucursal</a>
-                <a href="compre-licensia-digital">Compré una licensia digital</a>
-                  <a class="masinfo" href="masinfo">Más información</a>
-
-    </li>
-
-  </ul>
-
-      </div>
-
-<div class="FAQ">
- <div class="opciones-compras">
-    <ul>
-      <a href="pagos-facturacion"> Pagos y facturación </a>
-      <a href="retiro-envio">Retiro y envío </a>
-      <a href="Sucursales">Sucursales</a>
-      <a href="Garantía,-Servicio-Técnico-e-Instalaciones">Garantía, Servicio Técnico e Instalaciones </a>
-      <a href="credito-Garb">Credito-Garbarino</a>
-      <a href="Garbarino-agente-seguros">Garbarino Agente de Seguros</a>
-      <a href="Cambios-y-dev">Cambios y Devoluciones</a>
-      </ul>
-    </div>
-
-  <div class="preguntas-compras">
+    <div class="centrodeayuda">
      <ul>
-     <li> Consulta stock </li>
-     <li> ¿Puedo pagar con 2 medios de pago? </li>
-     <li> Solicitud Presupuesto </li>
-     <li> Fechas de entrega disponibles para envios a domicilio </li>
-     <li> Tengo un cupón de descuento </li>
-     <li> Conocé los costos de envio </li>
-     <li>¿Cómo comprar en garbarino.com?</li>
 
-     </ul>
-     </div>
- </div>
+        <li>
+         <div class="compras">
+
+           <div class="comprasicono">
+            <i class="fas fa-shopping-bag"></i>
+           </div>
+
+          <h1>Compras</h1>
+
+          <a href="#">Tengo un cupón de descuento</a>
+          <a href="#">Conocé los costos de envío</a>
+          <a href="#">¿Mi compra está registrada?</a>
+          <a href="#">¿Cómo comprar en garbarino.com?</a>
+          <a href="#">Promociones y medios de pago</a>
+          <a href="#">Retiro en Sucursal</a>
+          <a href="#">Compré una licencia digital</a>
+          <a class="masinfo" href="#">MÁS INFORMACIÓN</a>
+
+         </div>
+        </li>
+
+        <li>
+         <div class="pagos">
+
+           <div class="pagosicono">
+            <i class="fas fa-shopping-bag"></i>
+           </div>
+
+          <h1>PAGOS Y FACTURACIÓN</h1>
+
+          <a href="#">Informar un error en el cobro de mi compra</a>
+          <a href="#">Millas Aerolíneas Plus</a>
+          <a href="#">Solicitar una copia de mi factura</a>
+          <a href="#">Envío de facturas en productos de Garbarino Market</a>
+          <a href="#">Devoluciones en tarjetas de crédito y débito</a>
+          <a href="#">Solicitar una factura A</a>
+          <a class="masinfo" href="#">MÁS INFORMACIÓN</a>
+
+         </div>
+        </li>
+
+        <li>
+         <div class="entregas">
+
+           <div class="entregasicono">
+            <i class="fas fa-shopping-bag"></i>
+           </div>
+
+          <h1>ENTREGAS</h1>
+
+          <a href="#">Autorizar a una persona para recibir envío</a>
+          <a href="#">Plazo para retiro en sucursal</a>
+          <a href="#">Consultar donde se encuentra mi pedido</a>
+          <a href="#">Consultar si ya puedo retirar en sucursal</a>
+          <a href="#">Modificar la fecha de un envío a domicilio</a>
+          <a class="masinfo" href="#">MÁS INFORMACIÓN</a>
+
+         </div>
+        </li>
+
+        <li>
+         <div class="sucursales">
+
+           <div class="sucursalesicono">
+            <i class="fas fa-shopping-bag"></i>
+           </div>
+
+          <h1>SUCURSALES</h1>
+
+          <a href="#">Listados de Sucursales: Días y horarios</a>
+          <a class="masinfo" href="#">MÁS INFORMACIÓN</a>
+
+         </div>
+        </li>
+
+        <li>
+         <div class="garantias">
+
+           <div class="garantiasicono">
+            <i class="fas fa-shopping-bag"></i>
+           </div>
+
+          <h1>GARANTÍAS, SERVICIO TÉCNICO E INSTALACIONES</h1>
+
+          <a href="#">Informar demora en la instalación</a>
+          <a href="#">¿Qué es la garantía de reparación?</a>
+          <a href="#">Contratar la instalación de un aire acondicionado</a>
+          <a href="#">¿Qué es Compra segura?</a>
+          <a href="#">Requisitos necesarios para un reclamo de service</a>
+          <a href="#">Servicios técnicos oficiales</a>
+          <a class="masinfo" href="#">MÁS INFORMACIÓN</a>
+
+         </div>
+        </li>
+
+        <li>
+         <div class="cambios">
+
+           <div class="cambiosicono">
+            <i class="fas fa-shopping-bag"></i>
+           </div>
+
+          <h1>CAMBIOS Y DEVOLUCIONES</h1>
+
+          <a href="#">Solicitar un cambio de producto</a>
+          <a href="#">¿Tiene algún costo adicional?</a>
+          <a href="#">Políticas y condiciones generales</a>
+          <a href="#">¿Existe un tiempo máximo para solicitar un cambio o devolución?</a>
+          <a class="masinfo" href="#">MÁS INFORMACIÓN</a>
+
+         </div>
+        </li>
+
+      </ul>
+      </div>
 
 
-  </div>
+      <div class="preguntasfrecuentes">
 
+        <div class="preguntas">
+
+          <h1>Preguntas frecuentes</h1>
+
+          <ul>
+            <li><a href="#">Compras</a></li>
+            <li><a href="#">Pagos y Facturación</a></li>
+            <li><a href="#">Retiro y Envío</a></li>
+            <li><a href="#">Sucursales</a></li>
+            <li><a href="#">Garantía, Servicio Técnico e Instalaciones</a></li>
+            <li><a href="#">Crédito Garbarino</a></li>
+            <li><a href="#">Garbarino Agente de Seguros</a></li>
+            <li><a href="#"></a>Cambios y Devoluciones</li>
+          </ul>
+        </div>
+
+        <div class="preguntasresultado">
+
+          <h1>Pagos y Facturación</h1>
+
+          <details open>
+            <summary>Quiero cambiar Factura B por A</summary>
+            <p>Para solicitar un cambio de factura completá el siguiente formulario.</p>
+          </details>
+
+          <details open>
+            <summary>Informar un error en el cobro de mi compra</summary>
+            <p>Para informar un error en el cobro de tu compra completá el siguiente formulario y te estaremos contactando para ayudarte.</p>
+          </details>
+
+          <details open>
+            <summary>Millas Aerolíneas Plus</summary>
+            <p>Si tuviste un problema en la acreditación de las millas y querés realizar un reclamo, completá el siguiente formulario. Recordá completar tu número de socio Aerolíneas Plus en el campo “Consulta”.</p>
+          </details>
+
+          <details open>
+            <summary>Envío de facturas en productos de Garbarino Market</summary>
+            <p>Si compraste un producto de Garbarino Market, recibirás un e-mail con la factura una vez que la empresa la genere.</p>
+          </details>
+
+          <details open>
+            <summary>Devoluciones en tarjetas de crédito y débito</summary>
+            <p>Los tiempos de acreditación pueden demorar entre 72 hs hábiles hasta 10 días corridos, dependiendo la tarjeta que hayas utilizado y contemplando el cierre de tú resumen de cuenta.</p>
+          </details>
+
+          <details open>
+            <summary>Solicitar una factura A</summary>
+            <p>Ahora ya podés comprar en Garbarino.com y elegir en el paso 3 de la página de pago que tipo de factura querés, consumidor final o responsable inscripto (factura A).</p>
+          </details>
+        </div>
+
+
+      </div>
+
+      <img class="banner3" src="upload/banner3.webp" alt="">
+
+
+
+      <div class="contacteconnosotros">
+
+        <hgroup>
+          <h1>Contactate con nosotros</h1>
+          <h3>Seleccioná una opción</h3>
+        </hgroup>
+
+        <ul>
+
+
+          <li>
+            <div class="consultasicono">
+              <i class="fas fa-shopping-bag"></i>
+            </div>
+            <h2>Consultas</h2>
+          </li>
+
+          <li>
+            <div class="reclamosicono">
+              <i class="fas fa-shopping-bag"></i>
+            </div>
+            <h2>Reclamos</h2>
+          </li>
+
+          <li>
+            <div class="sugerenciasicono">
+              <i class="fas fa-shopping-bag"></i>
+            </div>
+            <h2>Sugerencias</h2>
+          </li>
+
+
+        </ul>
+
+      </div>
 
     <?php require_once('footer.php'); ?>
 
